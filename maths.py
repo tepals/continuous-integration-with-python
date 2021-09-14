9fe7ba42ddb640c501ed97e2d950f322e04bc658
Thera Pals <pals@delmic.com>
2021-09-14 11:06:07 +0200

    fix linting in maths.py

--- maths.py
+++ maths.py
@@ -1,12 +1,14 @@
-def multiple(first,second):
+def multiple(first, second):
     return first * second
 
-#def subs(a,b):
+
+# def subs(a,b):
 #    return a - b
 
-def divide(x,y):
-    return x / y 
 
-def add(x,y):
+def divide(x, y):
+    return x / y
+
+
+def add(x, y):
     return x + y
-
