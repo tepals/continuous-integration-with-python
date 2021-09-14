57a2a4fbba627f4160560cff8fe31f2a281b406b
Thera Pals <pals@delmic.com>
2021-09-14 13:45:49 +0200

    test apt-get installation of dependency

--- tests/test_multiple.py
+++ tests/test_multiple.py
@@ -1,5 +1,6 @@
 from maths import multiple, add
 import unittest
+import numpy
 
 
 class TestMaths(unittest.TestCase):
@@ -8,6 +9,9 @@ class TestMaths(unittest.TestCase):
 
     def test_add_3_4(self):
         assert add(3, 4) == 7
+
+    def test_numpy(self):
+        assert numpy.multiply(3, 4) == 12
 
     # def test_failing_test(self):
     #     assert add(3, 4) == 5
